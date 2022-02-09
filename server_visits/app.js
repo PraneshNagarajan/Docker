2900d82c31f27a7fc8c6573aa7f1b712e3555583
root <root@ip-172-31-94-196.ec2.internal>
2022-02-09 12:12:14 +0000

    get method

--- server_visits/app.js
+++ server_visits/app.js
@@ -1,66 +1,73 @@
 const express = require("express");
-const mongo = require("mongodb")
-const bodyParser = require('body-parser')
-const app = express()
+const mongo = require("mongodb");
+const bodyParser = require("body-parser");
+const app = express();
 const MongoClient = mongo.MongoClient;
-const PORT = 5050
-
+const PORT = 5050;
 
 //save credentials
-var url
+var url;
 
 // create application/json parser
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(jsonParser)
-app.use(urlencodedParser)
+app.use(jsonParser);
+app.use(urlencodedParser);
 
-
-app.get("/getHits", (req,res) => {
+app.get("/getHits", (req, res) => {
   MongoClient.connect(url, (err, mongoDB) => {
- 	if(err) {
-		res.send(String(err) + " / First login to access request")
-	}
-	  var server_hits =  mongoDB.db("server_hit")
-         var collection = server_hits.collection("hits")
-         collection.find({}).toArray((err, result) => {
- 		if (err) {
-		 res.send(String(err))
-		}
-		console.log(result.keys()) 
- 		res.send("No of times visited : " + result)
-	 })
-  })
-})
+    if (err) {
+      res.send(String(err) + " / First login to access request");
+    }
+    var server_hits = mongoDB.db("server_hit");
+    var collection = server_hits.collection("hits");
+    collection.find().then((result) => {
+      res.json({
+        result,
+      });
+    });
+  });
+});
 
-app.post("/insertNew" , (req,res) => {
-MongoClient.connect(url, (err, mongoDB) => {
-        if(err) {
-                res.send(String(err))
-        }
-	var server_hits =  mongoDB.db("server_hit")
-	var collection = server_hits.collection("hits")
-	collection.insertOne({ hits: 1 }, (err, result) => {
-             if (err) {  res.send(String(err)) }
-	     res.send("data inserted : " + result)
-	})
-  })
-})
+app.post("/insertNew", (req, res) => {
+  MongoClient.connect(url, (err, mongoDB) => {
+    if (err) {
+      res.send(String(err));
+    }
+    var server_hits = mongoDB.db("server_hit");
+    var collection = server_hits.collection("hits");
+    collection.insertOne({ hits: 1 }, (err, result) => {
+      if (err) {
+        res.send(String(err));
+      }
+      res.send("data inserted : " + result);
+    });
+  });
+});
 
-app.post("/login", (req,res) => {
-	MongoClient.connect( "mongodb://"+req.body.user+":"+req.body.password+"@mongoDB:27017" , (err, DB) => {
-		if(err) {
-			res.send(String(err))
-		} else {
-			url="mongodb://"+req.body.user+":"+req.body.password+"@mongoDB:27017"
-			res.send("login successfully")
-		}
-	})
-})
+app.post("/login", (req, res) => {
+  MongoClient.connect(
+    "mongodb://" + req.body.user + ":" + req.body.password + "@mongoDB:27017",
+    (err, DB) => {
+      if (err) {
+        res.send(String(err));
+      } else {
+        url =
+          "mongodb://" +
+          req.body.user +
+          ":" +
+          req.body.password +
+          "@mongoDB:27017";
+        res.send("login successfully");
+      }
+    }
+  );
+});
 
 app.listen(PORT, () => {
-    console.log("server is runing on port 5050")
-})
+  console.log("server is runing on port 5050");
+});
+
